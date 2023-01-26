d14fe9ad531a42302bdaa2688d375e95c7fa4a72
umbra-imafua <78512112+umbra-imafua@users.noreply.github.com>
2023-01-26 12:00:35 +0000

    Update blorboexec.sh

--- blorboexec.sh
+++ blorboexec.sh
@@ -4,4 +4,5 @@ ARG1=${1:""}
 
 BORBNAME="$(gpw 1 7)";
 convert -size 256x256 xc:#FFFFFF PNG24:/media/ubuntu-studio/DATA/ART/BLORBO/$BORBNAME.png;
-kolourpaint /media/ubuntu-studio/DATA/ART/BLORBO/$BORBNAME.png && $ARG1
+
+kolourpaint /media/ubuntu-studio/DATA/ART/BLORBO/$BORBNAME.png && echo "" | mutt -a "/media/ubuntu-studio/DATA/ART/BLORBO/$BORBNAME.png" -s "$BORBNAME, MyComputerMakesMePostTheseEveryTimeIgoToOpenChrome" -- recipient@mail.com msmtp trigger@applet.ifttt.com && $ARG1
